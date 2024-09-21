774b313a0f8541db2fea31e59b247793456d19e1
Nguyen Van Tam <tamnvhustcc@gmail.com>
2024-09-21 09:44:05 +0700

    update

--- components/layout/site-footer.tsx
+++ components/layout/site-footer.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import Link from "next/link";
-import Image from "next/image";
 
 import { footerLinks, siteConfig } from "@/config/site";
 import { cn } from "@/lib/utils";
